e0c46d079b58d38e9a335751640cfbed52967816
DiegoHrz <128105062+DiegoHrz@users.noreply.github.com>
2023-08-09 16:35:45 +0000

    Wed, Aug 9, 2023, 4:35 PM +00:00

--- Week1/population/population.c
+++ Week1/population/population.c
@@ -4,10 +4,25 @@
 int main(void)
 {
     // TODO: Prompt for start size
+    int start;
+    do{
+        start = get_int("Start size: ");
+
+    }while (start < 9);
 
     // TODO: Prompt for end size
+    int end;
+    do{
+        end = get_int("End size: ");
+
+    }while (end< 9);
 
     // TODO: Calculate number of years until we reach threshold
-
+    for(int i = 0;start >= end; i++){
+        start+= start/3
+        start-= start/4
+    }
     // TODO: Print number of years
+    printf("The number is %i\n", n);
+
 }
