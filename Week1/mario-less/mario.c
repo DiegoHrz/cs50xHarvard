c20c3a7bbc310402057c50d2694f82a754e54434
DiegoHrz <128105062+DiegoHrz@users.noreply.github.com>
2023-08-09 20:55:57 +0000

    Wed, Aug 9, 2023, 3:55 PM -05:00

--- Week1/mario-less/mario.c
+++ Week1/mario-less/mario.c
@@ -1,7 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    do {
+    int n = get_int("Height: ");
+  }while (n > 8 && n < 1) ;
+
+  for(int i = 0; i < n; i++){
+
+    for(int j = i+1; j < n; j++ ){
+      printf(" ");
+    }
+    for(int k = n - 1; (k - i) < n ; k++){
+      printf("#");
+    }
+    printf("\n");
+
+  }
 }
